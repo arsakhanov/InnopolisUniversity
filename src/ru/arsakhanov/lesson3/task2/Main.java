4c52434dc684b4ee05650b705d7cf350d259f8ba
arsakhanov <baki_12_95@mail.ru>
2020-03-24 19:23:05 +0300

    дополнительная задача для дз 3-2

--- src/ru/arsakhanov/lesson3/task2/Main.java
+++ src/ru/arsakhanov/lesson3/task2/Main.java
@@ -4,11 +4,10 @@ import java.util.Scanner;
 
 public class Main {
 
-
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int randomNumber = (int) (Math.random() * 100);
-
+        System.out.println(randomNumber);
         int userNumber;
         int difOfPrevNumber = randomNumber;     //Переменная для хранения разницы загаданного числа с предыдущим введенным числом
         int difOfNumber;                                      //Переменная для хранения разницы загаданного числа с введенным числом
@@ -16,6 +15,7 @@ public class Main {
         while (continueProgram) {
             System.out.println("Введите число от 1 до 100 или введите цифру 0, если хотите выйти из игры");
             userNumber = in.nextInt();
+
             if (userNumber == 0) {
                 continueProgram = false;
             } else {
@@ -23,10 +23,13 @@ public class Main {
                 if (userNumber == randomNumber) {
                     System.out.println("Вы отгадали число");
                     continueProgram = false;
-                } else {
-
+                } else if (userNumber == 101){
+                        randomNumber = (int) (Math.random() * 100);
+                        System.out.println("Измененное число " + randomNumber);
+                        System.out.println("Вы изменили число, которое должны отгадать!");
+                         }
+                    else {
                     difOfNumber = Math.abs(randomNumber - userNumber); //Здесь вычитывается разница между загаданным и введенным числами
-
                     if (difOfNumber < difOfPrevNumber) {
                         System.out.println("Горячо");
                     }else if (difOfNumber == difOfPrevNumber){
@@ -36,6 +39,7 @@ public class Main {
                     }
                     difOfPrevNumber = difOfNumber;
                 }
+
             }
 
         }
