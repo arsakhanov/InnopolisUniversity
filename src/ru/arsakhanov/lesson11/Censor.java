9bbe24c8bfd6f147f78c366cca62a2cdb354da9c
arsakhanov <baki_12_95@mail.ru>
2020-04-09 20:26:58 +0300

    Урок 11

---
+++ src/ru/arsakhanov/lesson11/Censor.java
@@ -0,0 +1,26 @@
+package ru.arsakhanov.lesson11;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Censor {
+
+
+
+    public static void main(String[] args) {
+
+        censorPattern();
+    }
+
+    public static void censorPattern(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите строку для проверки");
+        String text = in.nextLine();
+        Pattern censor = Pattern.compile("бяка(\\w*)");
+        Matcher replaceCensor = censor.matcher(text);
+        String newText = replaceCensor.replaceAll("вырезано цензурой ");
+        System.out.println(newText);
+    }
+
+}
