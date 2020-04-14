a7a0be29decb8be6c446cd4255009ffc5aed83cb
arsakhanov <baki_12_95@mail.ru>
2020-04-14 16:13:04 +0300

    Урок 8. Задача 1 переместил вывод строки поверх ввода радиуса круга

--- src/ru/arsakhanov/lesson8/task1/Example.java
+++ src/ru/arsakhanov/lesson8/task1/Example.java
@@ -1,14 +1,14 @@
 package ru.arsakhanov.lesson8.task1;
 
-import java.net.SecureCacheResponse;
+
 import java.util.Scanner;
 
 public class Example {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         final double PI = 3.14;
-        double circleRadius = in.nextDouble();
         System.out.println("Введите радиус круга:");
+        double circleRadius = in.nextDouble();
         double areaOfCircle = PI * (circleRadius * circleRadius);
         System.out.println("Площадь круга = " + areaOfCircle);
     }
