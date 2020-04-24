b3a5eb3baca38035f46e7b57229a0df8f647709f
arsakhanov <baki_12_95@mail.ru>
2020-04-24 14:00:23 +0300

    Урок 15. Добавил комментарии для документации

--- src/ru/arsakhanov/lesson15/Recursion.java
+++ src/ru/arsakhanov/lesson15/Recursion.java
@@ -1,25 +1,44 @@
 package ru.arsakhanov.lesson15;
 
 import java.io.File;
-import java.io.StringReader;
 
 public class Recursion {
 
     public static void main(String[] args) {
 
         File dir = new File("E://Games");
-        recursionDirectory(dir);
+        recursionDirectory(dir, 0);
     }
 
-    public static void recursionDirectory(File folder){
+    /**
+     *Метод выполняет рекурсию обхода всех каталогов и файлов в указанном каталоге
+     * @param folder здесь мы передаем через параметр путь к каталогу
+     * @param deep переменная для обработки глубины рекурсии, служит для того, чтобы увеличивать пробелы в рекурсии для подкаталогов
+     */
+    public static void recursionDirectory(File folder, int deep){
 
         File[] folderEntries = folder.listFiles(); //переда в массив файлы и папки в каталоге
         for(File entry : folderEntries){
             if(entry.isDirectory()){
-                System.out.println(entry.getName() + "\t Dir");
-                recursionDirectory(entry);
+                System.out.println(getSpaces(deep) + entry.getName() + "\t Dir");
+                recursionDirectory(entry, deep+1);
             }
-            else System.out.println(entry.getName() + "\t File");
+            else System.out.println(getSpaces(deep) + entry.getName() + "\t File");
         }
+    }
+
+    /**
+     *
+     * @param a задает глубины рекурсии, если мы идет вглубь, то эта переменная увеличивается на 1 и увеличивается количество пробелов
+     *          иначе количество пробелов уменьшается
+     * @return возвращает нам строку sb хранящая пробелы для глубины рекурсии
+     */
+    private static String getSpaces(int a){
+        StringBuilder sb = new StringBuilder();
+        while (a>0){
+            sb.append(" ");
+            a--;
+        }
+        return sb.toString();
     }
 }
