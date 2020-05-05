00894fa11f55c6656a66b82b28194d5a90b2e1e3
arsakhanov <baki_12_95@mail.ru>
2020-05-05 21:40:34 +0300

    Урок 19 исправлены некоторые мелкие детали

--- src/ru/arsakhanov/lesson19/Receipt.java
+++ src/ru/arsakhanov/lesson19/Receipt.java
@@ -8,35 +8,28 @@ import java.util.Scanner;
 public class Receipt {
 
     public static void main(String[] args) {
-        File file = new File("E://Downloads//products.txt");
+        File file = new File("resources//products.txt");
         try {
             Scanner scanner = new Scanner(file);
             Formatter f;
             f = new Formatter();
             System.out.println(f.format("%-16s%11s%15s%18s", "Наименование", "Цена", "Количество", "Стоимость"));
             System.out.println("=============================================================");
-            double quantity = 0;
-            double price = 0;
-            double cost =0;
+            double quantity;
+            double price;
+            double cost;
             double totalCost = 0;
-            String nameOfProduct = new String();
+            String nameOfProduct;
             while(scanner.hasNextLine()) {
-                f = new Formatter();
-                if (scanner.hasNextDouble()) {
-                    quantity = scanner.nextDouble();
-                    price = scanner.nextDouble();
-                    cost = quantity * price;
-                }
-                else nameOfProduct = scanner.nextLine();
+                nameOfProduct = scanner.nextLine();
+                quantity = Double.parseDouble(scanner.nextLine());
+                price = Double.parseDouble(scanner.nextLine());
+                cost = quantity * price;
                 totalCost += cost;
+                f = new Formatter();
                 f.format("%-16s %3$11.2f %13.2f %4$18.2f",nameOfProduct, quantity, price, cost);
-                if(quantity!=0 && price!=0 && cost!=0) {
-                    System.out.println(f);
-                    quantity = 0;
-                    price = 0;
-                    cost = 0;
+                System.out.println(f);
                 }
-            }
             f.close();
             System.out.println("=============================================================");
             System.out.printf("Итого %55.2f",totalCost);
